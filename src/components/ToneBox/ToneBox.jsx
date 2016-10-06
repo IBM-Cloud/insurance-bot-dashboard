ef56363cc9fc6e1b96730e24690fa99f390ee6a3
COLBY WILLIAMS <colby@colbycheeze.com>
2016-10-06 17:17:47 -0500

    Add recharts and circle progress
    temp save b/c I have to go

--- src/components/ToneBox/ToneBox.jsx
+++ src/components/ToneBox/ToneBox.jsx
@@ -1,28 +1,37 @@
 import React from 'react';
-import { Card } from 'material-ui/Card';
+import { Circle } from 'rc-progress';
+import { Card, CardMedia, CardHeader } from 'material-ui/Card';
+import { palette } from 'styles/muiTheme';
 import classes from './ToneBox.scss';
 
 
 const ToneBox = (props) => (
   <Card className={classes.container}>
-    {props.toneResult.length > 0 ?
-      <div>
-        Watson Analytics
-        <hr />
-        {props.toneResult.map((emotion, i) =>
-          <li key={i}>
-            <p>{emotion.text}: {(emotion.value * 100).toFixed(2)}%</p>
-          </li>
-        )}
-      </div>
-    :
+    <CardHeader title="Watson Analytics" />
+    {props.toneResult.length ?
+      {props.toneResult.map((emotion, i) =>
+        <div key={i}>
+          <p>{emotion.text}: {(emotion.value * 100).toFixed()}%</p>
+          <Circle
+            percent={(emotion.value * 100)}
+            strokeWidth="8"
+            trailWidth="8"
+            strokeColor={palette.graph1Color}
+            strokeLinecap="square"
+          />
+        </div>
+      )}
+      :
       <img alt="loading" src="/watson_anim.gif" height="180px" width="180px" />
-  }
+    }
   </Card>
   );
 
 ToneBox.propTypes = {
-  toneResult: React.PropTypes.array.isRequired,
-};
+  toneResult: React.PropTypes.arrayOf(React.PropTypes.shape({
+    text: React.PropTypes.string.isRequired,
+    value: React.PropTypes.number.isRequired,
+  })).isRequired,
+}
 
 export default ToneBox;
