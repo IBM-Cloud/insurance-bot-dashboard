78c4e8a045c6757fcd16dcc2abb72538dbddbc95
COLBY WILLIAMS <colby@colbycheeze.com>
2016-10-10 17:15:57 -0500

    Added first/last if available

--- src/components/ToneBox/ToneBox.jsx
+++ src/components/ToneBox/ToneBox.jsx
@@ -5,22 +5,24 @@ import { palette } from 'styles/muiTheme';
 import classes from './ToneBox.scss';
 
 
-const ToneBox = (props) => (
+const ToneBox = ({ toneResult }) => (
   <Card className={classes.container}>
-    <CardHeader title="Watson Analytics" />
-    {props.toneResult.length ?
-      {props.toneResult.map((emotion, i) =>
-        <div key={i}>
-          <p>{emotion.text}: {(emotion.value * 100).toFixed()}%</p>
-          <Circle
-            percent={(emotion.value * 100)}
-            strokeWidth="8"
-            trailWidth="8"
-            strokeColor={palette.graph1Color}
-            strokeLinecap="square"
-          />
-        </div>
-      )}
+    <CardHeader title="All Sentiments" />
+    {toneResult.length ?
+      <CardMedia>
+        {toneResult.map((emotion, i) =>
+          <div key={i}>
+            <p>{emotion.text}: {(emotion.value * 100).toFixed()}%</p>
+            <Circle
+              percent={(emotion.value * 100)}
+              strokeWidth="8"
+              trailWidth="8"
+              strokeColor={palette.graph1Color}
+              strokeLinecap="square"
+            />
+          </div>
+        )}
+      </CardMedia>
       :
       <img alt="loading" src="/watson_anim.gif" height="180px" width="180px" />
     }
@@ -32,6 +34,6 @@ ToneBox.propTypes = {
     text: React.PropTypes.string.isRequired,
     value: React.PropTypes.number.isRequired,
   })).isRequired,
-}
+};
 
 export default ToneBox;
