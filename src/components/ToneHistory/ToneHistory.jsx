e843ff15599a2c95eb48e494fd5df992d542032c
COLBY WILLIAMS <colby@colbycheeze.com>
2016-10-18 16:48:27 -0500

    Updated styling for History Graph

--- src/components/ToneHistory/ToneHistory.jsx
+++ src/components/ToneHistory/ToneHistory.jsx
@@ -13,6 +13,41 @@ import {
 import { palette } from 'styles/muiTheme';
 import classes from './ToneHistory.scss';
 
+const chartStyle = {
+  margin: {
+    top: 5,
+    left: 50,
+    right: 5,
+    bottom: 5,
+  },
+  xAxisPadding: {
+    left: 30,
+    right: 10,
+  },
+  lineType: 'natural', // http://recharts.org/api#Line
+};
+
+const formatPercent = data => {
+  const formatted = (data * 100).toFixed();
+  return formatted === '0' ? '' : `${formatted}%`;
+};
+
+const CustomLabel = props => (
+  <g>
+    <text
+      width={props.width}
+      height={props.height}
+      x={chartStyle.margin.left / 2}
+      y={props.y + chartStyle.margin.top}
+      className="recharts-text"
+      stroke="none"
+      fill={props.stroke}
+    >
+      <tspan x="25" dy="0.71em">Comments</tspan>
+    </text>
+  </g>
+);
+
 
 const ToneHistory = ({ toneHistory }) => (
   <Card className={classes.container}>
@@ -20,27 +55,45 @@ const ToneHistory = ({ toneHistory }) => (
     <div className={classes.content}>
       {toneHistory.length ?
         <ResponsiveContainer>
-          <LineChart width={672} height={125} data={toneHistory}>
-            <XAxis dataKey="name" interval={2} tickLine={false} />
-            <YAxis interval={1} tickLine={false} axisLine={false} />
+          <LineChart
+            data={toneHistory}
+            margin={chartStyle.margin}
+          >
+            <XAxis
+              label={<CustomLabel />}
+              dataKey="name"
+              interval={2}
+              tickLine={false}
+              padding={chartStyle.xAxisPadding}
+            />
+            <YAxis
+              tickFormatter={formatPercent}
+              domain={[0, 1]}
+              interval={1}
+              tickLine={false}
+              axisLine={false}
+            />
             <Tooltip />
             <Line
               dataKey="anger"
               stroke={palette.graph1Color}
-              type="natural"
+              type={chartStyle.lineType}
               strokeWidth={2}
+              formatter={formatPercent}
             />
             <Line
               dataKey="joy"
               stroke={palette.graph2Color}
-              type="natural"
+              type={chartStyle.lineType}
               strokeWidth={2}
+              formatter={formatPercent}
             />
             <Line
               dataKey="sadness"
               stroke={palette.graph3Color}
-              type="natural"
+              type={chartStyle.lineType}
               strokeWidth={2}
+              formatter={formatPercent}
             />
           </LineChart>
         </ResponsiveContainer>
