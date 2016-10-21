9619838b65e40fb20a5c80555d231b2d70fac12c
COLBY WILLIAMS <colby@colbycheeze.com>
2016-10-21 11:24:19 -0500

    Flagged messages show training needed warning

--- src/components/ChatBox/MessageList.jsx
+++ src/components/ChatBox/MessageList.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import moment from 'moment';
 import { CardMedia, CardText } from 'material-ui/Card';
+import FontIcon from 'material-ui/FontIcon';
+import { palette } from 'styles/muiTheme';
 import classes from './ChatBox.scss';
 
 const formatTime = (date) => moment(date).format('h:mma');
 
-const MessageList = ({ user, log, trainingNeeded }) => (
+const styles = {
+  icon: {
+    fontSize: '1.125rem',
+    marginRight: '0.5rem',
+  },
+};
+
+const MessageList = ({ user, log }) => (
   <CardMedia>
     <CardText style={{ padding: '0' }}>
       <ul className={classes.messageList}>
         {log.map((message, i) =>
-          <li key={i}>
-            <div className={`${classes.owner} ${classes.user} ${trainingNeeded ? classes.warning : ''}`}>
-              {user} | {formatTime(message.date)}
+          <li key={i} className={`${classes.message} ${message.trainingNeeded ? classes.warning : ''}`}>
+            <div className={`${classes.owner}`}>
+              {message.trainingNeeded ?
+                <span className={classes.trainingMsg}>
+                  <FontIcon style={styles.icon} className="fa fa-exclamation-triangle" color={palette.primary2Color} />
+                  TRAINING NEEDED
+                </span>
+                : ''
+              }
+              <div className={classes.user}>{user} | {formatTime(message.date)}</div>
             </div>
             <p className={`${classes.text} ${classes.user}`}>
               {message.inputText}
